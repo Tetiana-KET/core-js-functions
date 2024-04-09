48c995dc668bbe1253b887d389e2621649b1dc37
tatiana <belangelphone@gmail.com>
2024-04-09 10:29:08 +0300

    feat: solve retry, logger

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -144,8 +144,18 @@ function memoize(func) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  return () => {
+    let issue;
+    for (let i = 0; i <= attempts; i += 1) {
+      try {
+        return func();
+      } catch (error) {
+        issue = error;
+      }
+    }
+    return issue;
+  };
 }
 
 /**
@@ -171,8 +181,16 @@ function retry(/* func, attempts */) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return (...args) => {
+    const argsToString = args.map((arg) => JSON.stringify(arg)).join(',');
+    const startLog = `${func.name}(${argsToString}) starts`;
+    const endLog = `${func.name}(${argsToString}) ends`;
+    logFunc(startLog);
+    const result = func(...args);
+    logFunc(endLog);
+    return result;
+  };
 }
 
 /**
